8b93639e23d0699f46ca3d4f6f6638d72b74478a
Stian Thoresen <stian.thoresen@gmail.com>
2022-03-04 12:05:04 +0100

    SimpleJekyllSearch - Replaced lunr search with SimpleJekyllSearch for faster client side searching

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,40 +3,6 @@
 var markInstance = new Mark(document.getElementById('search-results'));
 
 (function () {
-  function displaySearchResults(results, store) {
-
-    var searchResults = document.getElementById('search-results');
-
-
-
-    if (results.length) { // Are there any results?
-      var appendString = '';
-
-       // for (var i = 0; i < results.length; i++) {  // Iterate over the results
-        for (var i = 0; i < results.length && i < 10; i++) {  // Iterate over the results
-        var item = store[results[i].ref];
-        contentPreview = getPreview(searchTerm, item.content, 170),
-        urlPreview = getPreview(searchTerm, item.url),
-        titlePreview = getPreview(searchTerm, item.title),
-        productPreview= getPreview(searchTerm, item.product);
-
-        // appendString += '<a href="' + item.url + '"><h4>' + item.title + '</h4></a>';
-        // appendString += '<p>' + contentPreview + '</p>';
-        appendString +=
-
-        "<h4 class='search-result-title'><a href='" + item.url.trim() + "?h=" + encodeURIComponent(searchTerm).replace(/'/g, "%27") + "'>" + titlePreview + "</a></h4>" +
-            "<p class='search-result-url'><small>" + "Produkt: " + productPreview + "</small></p>" +
-            "<p class='search-result-snippet'>" + contentPreview + "</p>";
-
-      }
-
-      searchResults.innerHTML = appendString, performMark();
-    } else {
-      // this is a hack for the inability to submit different language strings here
-      searchResults.innerHTML = '<span style="color: red"><i class="fa fa-times" aria-hidden="true"></i><i class="fa fa-times" aria-hidden="true"></i><i class="fa fa-times" aria-hidden="true"></i><i class="fa fa-times" aria-hidden="true"></i><i class="fa fa-times" aria-hidden="true"></i></span><p><br/>{{site.uistring.no_search_results_found}}</p>';
-    }
-  }
-
   function getQueryVariable(variable) {
     var query = window.location.search.substring(1);
     var vars = query.split('&');
@@ -52,7 +18,8 @@ var markInstance = new Mark(document.getElementById('search-results'));
 
   function getPreview(searchTerm, content, previewLength) {
       previewLength = previewLength || (content.length * 2);
-
+      // console.log('getpreview');
+      // console.log(content);
       var parts = searchTerm.split(" "),
           loweredContent = content.toLowerCase(),
           match = content.toLowerCase().indexOf(searchTerm.toLowerCase()),
@@ -85,7 +52,7 @@ var markInstance = new Mark(document.getElementById('search-results'));
           }
 
           // Highlight query parts
-          preview = preview.replace(new RegExp("(" + parts.join("|") + ")", "gi"), "<strong>$1</strong>");
+          preview = preview.replace(new RegExp("(" + parts.join("|") + ")", "gi"), "<div style=\"background-color:yellow; display: inline\"><strong>$1</strong></div>");
       } else {
           // Use start of content if no match found
           preview = content.substring(0, previewLength).trim() + (content.length > previewLength ? "..." : "");
@@ -102,40 +69,61 @@ var markInstance = new Mark(document.getElementById('search-results'));
           markInstance.mark(searchTerm);
       }
 
+    document.addEventListener('DOMContentLoaded', function() {
+      initializeJekyllSearch();
+      var value = document.getElementById('search-box').value;
+      if(value) {
+        // Use this to search
+        setTimeout(() => {
+          window.simpleJekyllSearch.search(value);
+        }, 200)
+      }
+    }, false);
+
+    function initializeJekyllSearch() {
+      window.simpleJekyllSearch = SimpleJekyllSearch({
+        searchInput: document.getElementById('search-box'),
+        resultsContainer: document.getElementById('search-results'),
+        searchResultTemplate: "<h4 class='search-result-title'><a href={url}>{title}</a></h4>" +
+            "<p class='search-result-url'><small>" + "Produkt: {product}</small></p>" +
+            "<div><p class='search-result-snippet'>{content}</p></div>",
+        json: '/search.json',        
+        debounceTime: 200,
+        templateMiddleware: function(prop, value, template) {
+          if(prop === 'content') {
+            if(value) {
+              console.log('added: ' + value);
+              return getPreview(document.getElementById('search-box').value, value, 170);
+            }            
+            template = '';
+            console.log('removed: ' + value);
+            return template;
+          } else if(prop === 'product') {
+            if(value) {
+              return value;
+            }
+            return '';
+          } else if(prop === 'title') {
+              return getPreview(document.getElementById('search-box').value, value, 170);
+          }
+
+          return value;
+        },
+        // sortMiddleware: function(a, b) {
+        //   var astr = String(a.title) + "-" + String(a.content);
+        //   var bstr = String(b.title) + "-" + String(b.content);
+        //   return astr.localeCompare(bstr)
+        // },
+        success: function() {
+          //document.getElementById('search-results').innerHTML = document.getElementById('search-results').innerHTML, markInstance.mark(document.getElementById('search-box').value);
+          console.log('success search');
+        }
+        });
+    }
 
   if (searchTerm) {
     document.getElementById('search-box').setAttribute("value", searchTerm);
-
-    //document.getElementById('search-box').setAttribute("value", searchTerm);
-
-    // Initalize lunr with the fields it will be searching on. I've given title
-    // a boost of 10 to indicate matches on this field are more important.
-    var idx = lunr(function () {
-      this.field('id');
-      this.field('title', { boost: 10 });
-      this.field('product');
-      this.field('tags');
-      this.field('content');
-
-    });
-
-    for (var key in window.store) { // Add the data to lunr
-      idx.add({
-        'id': key,
-        'title': window.store[key].title,
-        'tags': window.store[key].tags,
-        'content': window.store[key].content,
-        'product': window.store[key].product
-      });
-      // Listen to input and option changes
-
-
-      var results = idx.search(searchTerm); // Get lunr to perform a search
-      displaySearchResults(results, window.store, searchTerm); // We'll write this in the next section
-
     }
-  }
-
 }
 
 )();
